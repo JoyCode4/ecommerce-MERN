625387201da7a594f3e33fb82f47d2ccd4d1f26b
JoyCode4 <joywadhonkar4@gmail.com>
2024-04-04 18:36:17 +0530

    1.Protected Components is created for redirecting to the login if user is not Login 2.auth reducer is created 3.authAPi and authSlice create two functionalities of createUser and checkUser 4.Validation to the form is provided by using react hook form and also make changes for navigation isf user not loggedIn

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -1,8 +1,27 @@
+import axios from "axios";
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
-  return new Promise(async (resolve) => {
-    const response = await fetch("http://localhost:8080");
+export async function createUser(userData) {
+  const response = await axios.post("http://localhost:8080/users", userData, {
+    headers: { "Content-Type": "application/json" },
+  });
+  return response;
+}
+
+export async function checkUser(loginInfo) {
+  return new Promise(async (resolve, reject) => {
+    const email = loginInfo.email;
+    const password = loginInfo.password;
+    const response = await fetch("http://localhost:8080/users?email=" + email);
     const data = await response.json();
-    resolve({ data });
+    console.log(data);
+    if (data.length) {
+      if (data[0].password === password) {
+        resolve({ data: data[0] });
+      } else {
+        reject({ message: "Invalid Credentials" });
+      }
+    } else {
+      reject({ message: "User not found" });
+    }
   });
 }
